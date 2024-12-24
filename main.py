469065c70fedb6b637b51b37d748ba960ee232d5
Maor Sasson <maors@mprest.com>
2024-12-24 10:18:11 +0200

    su

--- main.py
+++ main.py
@@ -10,20 +10,93 @@ def checker():
     else:
         main_label.config(text="Fuck Hard Refael")
 
-def system_check_health():
-    main_label.config(text='System Health')   # Change the main label
+def system_check_health_all_system():
+    main_label.config(text='System Health (All System)')   # Change the main label
     main_label.place(x=170,y=80)
     section_label.config(text='system_check_health') # Change the section label
     section_label.place(x=270,y=10)
-    b1.config(text='Ping Test', command=ping_test)
-    b1.place(x=145,y=350)
-    # b1.place_forget()                             # Make Button gone
-    b2.config(text='Softwares Checker', command=ping_test)
-    b2.place(x=145,y=400)
-    # b2.place_forget()                             # Make Button gone
+    b1.place_forget()                             # Make Button gone
+    b2.place_forget()                             # Make Button gone
     b3.place_forget()                             # Make Button gone
     b4.place_forget()                             # Make Button gone
+    b5.place_forget()                             # Make Button gone
+    b6.place_forget()                             # Make Button gone
+
+    b7 = ttk.Button(root, text="Back", bootstyle=(LIGHT, OUTLINE), command=lambda:back(cb1,cb2,cb3,b7,b8))
+    b7.place(x=10,y=540)
+
+    b8 = ttk.Button(root, text="Start", bootstyle=(LIGHT, OUTLINE), command=lambda:back(cb1,cb2,cb3))
+    b8.place(x=310,y=500)
+
+    # Create Checkbutton
+    ping_var = IntVar()
+    cb1 = ttkb.Checkbutton(bootstyle="LIGHT, round-toggle", text="Ping Test", variable=ping_var, onvalue=1, offvalue=0, command=checker)
+    cb1.place(x=300,y=300)
+
+    softwares_var = IntVar()
+    cb2 = ttkb.Checkbutton(bootstyle="LIGHT, round-toggle", text="Softwares Test", variable=softwares_var, onvalue=1, offvalue=0, command=checker, state=DISABLED)
+    cb2.place(x=300,y=320)
+
+    older_spyder_var = IntVar()
+    cb3 = ttkb.Checkbutton(bootstyle="LIGHT, round-toggle", text="Older Spyder Software Test", variable=older_spyder_var, onvalue=1, offvalue=0, command=checker)
+    cb3.place(x=300,y=340)
+
+
+def system_check_health_self():
+    main_label.config(text='System Health (Self)')   # Change the main label
+    main_label.place(x=170,y=80)
+    section_label.config(text='system_check_health') # Change the section label
+    section_label.place(x=270,y=10)
+    b1.place_forget()                             # Make Button gone
+    b2.place_forget()                             # Make Button gone
+    b3.place_forget()                             # Make Button gone
+    b4.place_forget()                             # Make Button gone
+    b5.place_forget()                             # Make Button gone
+    b6.place_forget()                             # Make Button gone
+
+    b7 = ttk.Button(root, text="Back", bootstyle=(LIGHT, OUTLINE), command=lambda:back(cb1,cb2,cb3,b7,b8))
+    b7.place(x=10,y=540)
+
+    b8 = ttk.Button(root, text="Start", bootstyle=(LIGHT, OUTLINE), command=lambda:back(cb1,cb2,cb3))
+    b8.place(x=310,y=500)
+
+    # Create Checkbutton
+    ping_var = IntVar()
+    cb1 = ttkb.Checkbutton(bootstyle="LIGHT, round-toggle", text="Ping Test", variable=ping_var, onvalue=1, offvalue=0, command=checker)
+    cb1.place(x=300,y=300)
+
+    softwares_var = IntVar()
+    cb2 = ttkb.Checkbutton(bootstyle="LIGHT, round-toggle", text="Softwares Test", variable=softwares_var, onvalue=1, offvalue=0, command=checker)
+    cb2.place(x=300,y=320)
+
+    older_spyder_var = IntVar()
+    cb3 = ttkb.Checkbutton(bootstyle="LIGHT, round-toggle", text="Older Spyder Software Test", variable=older_spyder_var, onvalue=1, offvalue=0, command=checker)
+    cb3.place(x=300,y=340)
+
+def back(cb1,cb2,cb3,b7,b8):
+    main_label.config(text='Spyder SU')
+    main_label.place(x=200,y=80)
+
+    section_label.config(text='Main')
+    section_label.place(x=310,y=10)
+
+    cb1.place_forget()
+    cb2.place_forget()
+    cb3.place_forget()
     
+
+    b1.place(x=200,y=300)
+    b2.place(x=200,y=340)
+    b3.place(x=200,y=380)
+    b4.place(x=200,y=420)
+    b5.place(x=200,y=460)
+    b6.place(x=10,y=540)
+    
+    b7.place_forget()
+    b8.place_forget()
+
+
+
 def ping_test():
     b1.config(text='START')
     b1.place(x=145,y=350)
@@ -33,7 +106,7 @@ root = ttkb.Window(themename="vapor")                          #Set the root var
 root.geometry('700x600')                #Set the size of the window
 root.resizable(False,False)             #Set the option to resize the window to inactive
 root.title('Spyder Software Update')    #Set the tile of the app
-root.iconbitmap("C:\Python Projects\Spyder SU\images\mprest.ico")
+root.iconbitmap("C:\Work\Spyder-SU\images\mprest.ico")
 
 
 # Set a background image for the app
@@ -58,7 +131,7 @@ help_menu.add_command(label='2. About', command=checker)
 
 
 # Create Labels
-main_label = ttkb.Label(text='Spyder SU', font=("Helvetica", 28), bootstyle='SUCCESS')
+main_label = ttkb.Label(text='Spyder SU', font=("Helvetica", 28), bootstyle='LIGHT')
 main_label.place(x=200,y=80)
 
 section_label = ttkb.Label(text='Main', bootstyle="light")
@@ -67,17 +140,23 @@ section_label.place(x=310,y=10)
 
 
 # Create Buttons
-b1 = ttk.Button(root, text="System Health Check", bootstyle=(SUCCESS, OUTLINE), command=system_check_health)
-b1.place(x=245,y=350)
+b1 = ttk.Button(root, text="System Health Check (All System)", bootstyle=(LIGHT, OUTLINE), command=system_check_health_all_system)
+b1.place(x=200,y=300)
 
-b2 = ttk.Button(root, text="Spyder Software Installation", bootstyle=(LIGHT, OUTLINE), state=DISABLED)
-b2.place(x=220,y=405)
+b2 = ttk.Button(root, text="System Health Check (Self)", bootstyle=(LIGHT, OUTLINE), command=system_check_health_self)
+b2.place(x=200,y=340)
 
-b3 = ttk.Button(root, text="ThirdParty Softwares Installation", bootstyle=(LIGHT, OUTLINE))
-b3.place(x=205,y=460)
+b3 = ttk.Button(root, text="Spyder Software Installation (All System)", bootstyle=(LIGHT, OUTLINE), state=DISABLED)
+b3.place(x=200,y=380)
 
-b4 = ttk.Button(root, text="Exit", bootstyle=(LIGHT, OUTLINE))
-b4.place(x=310,y=515)
+b4 = ttk.Button(root, text="Spyder Software Installation (Self)", bootstyle=(LIGHT, OUTLINE), state=DISABLED)
+b4.place(x=200,y=420)
+
+b5 = ttk.Button(root, text="ThirdParty Softwares Installation", bootstyle=(LIGHT, OUTLINE))
+b5.place(x=200,y=460)
+
+b6 = ttk.Button(root, text="Exit", bootstyle=(LIGHT, OUTLINE), command=exit)
+b6.place(x=10,y=540)
 
 # Create Checkbutton
 # var = IntVar()
