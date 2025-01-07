0a4b8fdc32fedaf9faf9a0f6fcc3af5074f030cd
Maor Sasson <maors@mprest.com>
2025-01-07 19:53:29 +0200

    network almost done

--- main.py
+++ main.py
@@ -2,13 +2,78 @@
 from modules import *
 from app_functions import *
 from ping import *
+import threading
+from tkinter import messagebox
+import socket
+from softwares import *
 
 # Functions
-def checker():
-    if var.get() == 1:
-        main_label.config(text="Fuck Refael")
+# Functions for networking check and threading
+def update_progressbar(pb, progress_value):
+    pb['value'] = progress_value
+
+
+def networking(battnumber, project, set, b9):
+    global pb_label
+    global pb
+    # Create a progress bar and label
+    if set == 'Main+Backup':
+        b9.configure(bootstyle='info')
+        pb = ttk.Progressbar(root, bootstyle='info', maximum=200, mode="determinate", length=200, value=0)
+        pb.place(x=255, y=550)
+        pb_label = ttk.Label(root, text="Networking Check In Progress")
+        pb_label.place(x=230, y=570)
+        pb_label.configure(text="Networking Check In Progress")
     else:
-        main_label.config(text="Fuck Hard Refael")
+        b9.configure(bootstyle='info')
+        pb = ttk.Progressbar(root, bootstyle='info', maximum=100, mode="inte", length=200, value=0)
+        pb.place(x=255, y=550)
+        pb_label = ttk.Label(root, text="Networking Check In Progress")
+        pb_label.place(x=230, y=570)
+        pb_label.configure(text="Networking Check In Progress")
+
+    # Create a thread to run the ping function
+    def ping_thread():
+        # Run the ping function and get the result
+        result = ping(battnumber, project, set, update_progressbar, pb)
+
+        # Update the progress bar and label
+        if result == 0:
+            update_progressbar(pb, 100)  # Assuming ping success should update to 100%
+            messagebox.showinfo("Ping Success", "All pings were successful.")
+            b9.configure(bootstyle='SUCCESS')
+            pb_label.place_forget()
+            pb.place_forget()            
+        else:
+            b9.configure(bootstyle='DANGER')
+            pb_label.place_forget()
+            pb.place_forget()
+            messagebox.showerror("Ping Failed", "There failures in the ping test.")
+
+    # Start the thread to run the ping function
+    threading.Thread(target=ping_thread, daemon=True).start()
+
+def software_check(battnumber,project,set,b10):
+    if set == 'Main+Backup':
+        b10.configure(bootstyle='info')
+        pb = ttk.Progressbar(root, bootstyle='info', maximum=200, mode="determinate", length=200, value=0)
+        pb.place(x=255, y=550)
+        pb_label = ttk.Label(root, text="Softwares Check In Progress")
+        pb_label.place(x=230, y=570)
+        pb_label.configure(text="Softwares Check In Progress")
+    else:
+        b10.configure(bootstyle='info')
+        pb = ttk.Progressbar(root, bootstyle='info', maximum=100, mode="inte", length=200, value=0)
+        pb.place(x=255, y=550)
+        pb_label = ttk.Label(root, text="Softwares Check In Progress")
+        pb_label.place(x=230, y=570)
+        pb_label.configure(text="Softwares Check In Progress")
+
+    def software_thread():
+        # Run the ping function and get the result
+        result = run_wmi_query()
+
+    threading.Thread(target=software_thread, daemon=True).start()
 
 def system_check_health_all_system():
     main_label.config(text='System Health')   # Change the main label
@@ -24,7 +89,7 @@ def system_check_health_all_system():
     #b5.place_forget()                             # Make Button gone
     b6.place_forget()                             # Make Button gone
 
-    b7 = ttk.Button(root, text="Back", bootstyle=(DARK, OUTLINE), command=lambda:back(b9,b10,b11,b12,b7,cb2,cb1,racks_label,battey_label,projects_label,cb3,secondary_label))
+    b7 = ttk.Button(root, text="Back", bootstyle=(DARK, OUTLINE), command=lambda:back(b9,b10,b11,b12,b7,cb2,cb1,sets_label,battey_label,projects_label,cb3,secondary_label))
     b7.place(x=10,y=540)
 
     #b8 = ttk.Button(root, text="Start", bootstyle=(DARK, OUTLINE), command=lambda:back(cb1,cb2,cb3))
@@ -39,36 +104,38 @@ def system_check_health_all_system():
     cb1.place(x=240, y=220)
     cb1.current(0)
     
-    racks_label = ttkb.Label(text='Select Number of racks:', font=("Helvetica", 11), bootstyle='DARK')
-    racks_label.place(x=240,y=270)
-
-    racks = ['1R','2R']
-
-    cb2 = ttkb.Combobox(root, bootstyle='dark', values=racks)
-    cb2.place(x=240, y=300)
-    cb2.current(0)
 
     projects = ['Spyder','All-in-One',]
 
     projects_label = ttkb.Label(text='Select Project:', font=("Helvetica", 11), bootstyle='DARK')
-    projects_label.place(x=240,y=350)
+    projects_label.place(x=240,y=270)
 
     cb3 = ttkb.Combobox(root, bootstyle='dark', values=projects)
-    cb3.place(x=240, y=380)
+    cb3.place(x=240, y=300)
     cb3.current(0)
+    
+    sets_label = ttkb.Label(text='Select Number of racks:', font=("Helvetica", 11), bootstyle='DARK')
+    sets_label.place(x=240,y=350)
+    if cb3.get() == 'All-in-One':
+        sets = ['Main']
+    else: 
+        sets = ['Main','Backup','Main+Backup']
 
+    cb2 = ttkb.Combobox(root, bootstyle='dark', values=sets)
+    cb2.place(x=240, y=380)
+    cb2.current(0)
 
     # Create Checkbutton
-    b9 = ttk.Button(root, text="Networking", bootstyle=(DARK, OUTLINE), command=lambda:ping(cb1.get()))
+    b9 = ttk.Button(root, text="Networking", bootstyle=(DARK, OUTLINE), command=lambda:networking(cb1.get(),cb3.get(),cb2.get(),b9))
     b9.place(x=240,y=450)
 
-    b10 = ttk.Button(root, text="Softwares", bootstyle=(DARK, OUTLINE), command=checker)
+    b10 = ttk.Button(root, text="Softwares", bootstyle=(DARK, OUTLINE), command=lambda:software_check(cb3.get(),cb2.get(),b10))
     b10.place(x=370,y=450)
 
-    b11 = ttk.Button(root, text="Permissions", bootstyle=(DARK, OUTLINE), command=checker)
+    b11 = ttk.Button(root, text="Permissions", bootstyle=(DARK, OUTLINE), command=networking)
     b11.place(x=240,y=495)
 
-    b12 = ttk.Button(root, text="Disk Space", bootstyle=(DARK, OUTLINE), command=checker)
+    b12 = ttk.Button(root, text="Disk Space", bootstyle=(DARK, OUTLINE), command=networking)
     b12.place(x=370,y=495)
     
 def system_check_health_self():
@@ -87,7 +154,7 @@ def system_check_health_self():
     #b5.place_forget()                             # Make Button gone
     b6.place_forget()                             # Make Button gone
 
-    b7 = ttk.Button(root, text="Back", bootstyle=(DARK, OUTLINE), command=lambda:back(b9,b10,b11,b12,b7,cb2,cb1,racks_label,battey_label,projects_label,cb3,secondary_label))
+    b7 = ttk.Button(root, text="Back", bootstyle=(DARK, OUTLINE), command=lambda:back(b9,b10,b11,b12,b7,cb2,cb1,sets_label,battey_label,projects_label,cb3,secondary_label))
     b7.place(x=10,y=540)
 
     #b8 = ttk.Button(root, text="Start", bootstyle=(DARK, OUTLINE), command=lambda:back(cb1,cb2,cb3))
@@ -102,35 +169,35 @@ def system_check_health_self():
     cb1.place(x=240, y=220)
     cb1.current(0)
     
-    racks_label = ttkb.Label(text='Select Component', font=("Helvetica", 11), bootstyle='DARK')
-    racks_label.place(x=240,y=270)
+    sets_label = ttkb.Label(text='Select Component', font=("Helvetica", 11), bootstyle='DARK')
+    sets_label.place(x=240,y=350)
 
-    racks = ['CCU1','CCU2',"ICS1","ICS2","DRS1","DRS2","OC1","OC2","COP"]
+    sets = ['CCU1','CCU2',"ICS1","ICS2","DRS1","DRS2","OC1","OC2","COP"]
 
-    cb2 = ttkb.Combobox(root, bootstyle='dark', values=racks)
-    cb2.place(x=240, y=300)
+    cb2 = ttkb.Combobox(root, bootstyle='dark', values=sets)
+    cb2.place(x=240, y=380)
     cb2.current(0)
 
     projects = ['Spyder','All-in-One',]
 
     projects_label = ttkb.Label(text='Select Project:', font=("Helvetica", 11), bootstyle='DARK')
-    projects_label.place(x=240,y=350)
+    projects_label.place(x=240,y=270)
 
     cb3 = ttkb.Combobox(root, bootstyle='dark', values=projects)
-    cb3.place(x=240, y=380)
+    cb3.place(x=240, y=300)
     cb3.current(0)
 
     # Create Checkbutton
     b9 = ttk.Button(root, text="Networking", bootstyle=(DARK, OUTLINE), command=lambda:ping(cb1.get()))
     b9.place(x=240,y=450)
 
-    b10 = ttk.Button(root, text="Softwares", bootstyle=(DARK, OUTLINE), command=checker)
+    b10 = ttk.Button(root, text="Softwares", bootstyle=(DARK, OUTLINE), command=networking)
     b10.place(x=370,y=450)
 
-    b11 = ttk.Button(root, text="Permissions", bootstyle=(DARK, OUTLINE), command=checker)
+    b11 = ttk.Button(root, text="Permissions", bootstyle=(DARK, OUTLINE), command=networking)
     b11.place(x=240,y=495)
 
-    b12 = ttk.Button(root, text="Disk Space", bootstyle=(DARK, OUTLINE), command=checker)
+    b12 = ttk.Button(root, text="Disk Space", bootstyle=(DARK, OUTLINE), command=networking)
     b12.place(x=370,y=495)
 
 def system_installation_Self():
@@ -183,10 +250,10 @@ def system_installation_Self():
     cb3.current(0)
 
     # Create Checkbutton
-    b9 = ttk.Button(root, text="Spyder Software", bootstyle=(DARK, OUTLINE), command=checker)
+    b9 = ttk.Button(root, text="Spyder Software", bootstyle=(DARK, OUTLINE), command=networking)
     b9.place(x=200,y=450)
 
-    b10 = ttk.Button(root, text="Third Party Softwares", bootstyle=(DARK, OUTLINE), command=checker)
+    b10 = ttk.Button(root, text="Third Party Softwares", bootstyle=(DARK, OUTLINE), command=networking)
     b10.place(x=370,y=450)
 
     # b11 = ttk.Button(root, text="Permissions", bootstyle=(DARK, OUTLINE), command=checker)
@@ -195,7 +262,7 @@ def system_installation_Self():
     # b12 = ttk.Button(root, text="Disk Space", bootstyle=(DARK, OUTLINE), command=checker)
     # b12.place(x=370,y=495)
 
-def back2(b9,b10,b7,cb2,cb1,battey_label,racks_label,projects_label,cb3,secondary_label):
+def back2(b9,b10,b7,cb2,cb1,battey_label,sets_label,projects_label,cb3,secondary_label):
     main_label.config(text='Spyder SU')
     main_label.place(x=200,y=80)
     
@@ -207,10 +274,10 @@ def back2(b9,b10,b7,cb2,cb1,battey_label,racks_label,projects_label,cb3,secondar
     b9.place_forget()
     b10.place_forget()
 
-    b1.place(x=200,y=300)
-    b2.place(x=200,y=340)
-    b3.place(x=200,y=380)
-    b4.place(x=200,y=420)
+    b1.place(x=190,y=300)
+    b2.place(x=190,y=360)
+    b3.place(x=190,y=420)
+    b4.place(x=190,y=480)
     #b5.place(x=200,y=460)
     b6.place(x=10,y=540)
     
@@ -220,11 +287,11 @@ def back2(b9,b10,b7,cb2,cb1,battey_label,racks_label,projects_label,cb3,secondar
     cb1.place_forget()
     battey_label.place_forget()
     cb2.place_forget()
-    racks_label.place_forget()
+    sets_label.place_forget()
     projects_label.place_forget()
     cb3.place_forget()
 
-def back(b9,b10,b11,b12,b7,cb2,cb1,battey_label,racks_label,projects_label,cb3,secondary_label):
+def back(b9,b10,b11,b12,b7,cb2,cb1,battey_label,sets_label,projects_label,cb3,secondary_label):
     main_label.config(text='Spyder SU')
     main_label.place(x=200,y=80)
     
@@ -238,10 +305,10 @@ def back(b9,b10,b11,b12,b7,cb2,cb1,battey_label,racks_label,projects_label,cb3,s
     b11.place_forget()
     b12.place_forget()
 
-    b1.place(x=200,y=300)
-    b2.place(x=200,y=340)
-    b3.place(x=200,y=380)
-    b4.place(x=200,y=420)
+    b1.place(x=190,y=300)
+    b2.place(x=190,y=360)
+    b3.place(x=190,y=420)
+    b4.place(x=190,y=480)
     #b5.place(x=200,y=460)
     b6.place(x=10,y=540)
     
@@ -251,9 +318,10 @@ def back(b9,b10,b11,b12,b7,cb2,cb1,battey_label,racks_label,projects_label,cb3,s
     cb1.place_forget()
     battey_label.place_forget()
     cb2.place_forget()
-    racks_label.place_forget()
+    sets_label.place_forget()
     projects_label.place_forget()
     cb3.place_forget()
+    # remove_pb()
 
 
 # Create the main windows of the app
@@ -275,15 +343,14 @@ root.config(menu=app_menu)
 # Create a open menu item
 open_menu = tk.Menu(app_menu)
 app_menu.add_cascade(label='Open', menu=open_menu)
-open_menu.add_command(label='1. Softwares Installation Guid', command=checker)
-open_menu.add_command(label='2. Softwares List', command=checker)
+open_menu.add_command(label='1. Softwares Installation Guid', command=networking)
+open_menu.add_command(label='2. Softwares List', command=networking)
 
 # Create a help menu item
 help_menu = tk.Menu(app_menu)
 app_menu.add_cascade(label='Help', menu=help_menu)
-help_menu.add_command(label='1. mPrest instegrators members', command=checker)
-help_menu.add_command(label='2. About', command=checker)
-
+help_menu.add_command(label='1. mPrest instegrators members', command=networking)
+help_menu.add_command(label='2. About', command=networking)
 
 # Create Labels
 main_label = ttkb.Label(text='Spyder SU', font=("Helvetica", 28), bootstyle='DARK')
@@ -292,20 +359,27 @@ main_label.place(x=200,y=80)
 section_label = ttkb.Label(text='Main', bootstyle="DARK")
 section_label.place(x=310,y=10)
 
+local_ip = socket.gethostbyname(socket.gethostname())
+hostname = socket.gethostname()
+runner_info_label = ttkb.Label(text=f'''
+                                {hostname}
+                               {local_ip}''')
+runner_info_label.place(relx=-0.2, rely=-0.050)
+# runner_info_label.place(x=400,y=50)
 
 
 # Create Buttons
-b1 = ttk.Button(root, text="System Health Check (All System)", bootstyle=(DARK, OUTLINE), command=system_check_health_all_system)
-b1.place(x=200,y=300)
+b1 = ttk.Button(root, text="System Health Check (All System)", bootstyle=(DARK, OUTLINE),width=30, command=system_check_health_all_system)
+b1.place(x=190,y=300)
 
-b2 = ttk.Button(root, text="System Health Check (Self)", bootstyle=(DARK, OUTLINE), command=system_check_health_self)
-b2.place(x=200,y=340)
+b2 = ttk.Button(root, text="System Health Check (Self)", bootstyle=(DARK, OUTLINE),width=30, command=system_check_health_self)
+b2.place(x=190,y=360)
 
-b3 = ttk.Button(root, text="System Installation (All System)", bootstyle=(DARK, OUTLINE))
-b3.place(x=200,y=380)
+b3 = ttk.Button(root, text="System Installation (All System)", bootstyle=(DARK, OUTLINE),width=30)
+b3.place(x=190,y=420)
 
-b4 = ttk.Button(root, text="System Software Installation (Self)", bootstyle=(DARK, OUTLINE), command=system_installation_Self)
-b4.place(x=200,y=420)
+b4 = ttk.Button(root, text="System Software Installation (Self)", bootstyle=(DARK, OUTLINE),width=30, command=system_installation_Self)
+b4.place(x=190,y=480)
 
 #b5 = ttk.Button(root, text="ThirdParty Softwares Installation", bootstyle=(DARK, OUTLINE))
 #b5.place(x=200,y=460)
--- ping.py
+++ ping.py
@@ -1,22 +1,74 @@
 import subprocess
-import platform
-import threading
+import datetime
+import os
 
-async def ping(batt_number):
-    print(batt_number)
-    command = ["ping", "-n", "3", f"8.8.{batt_number}.8"]
-    try:
-        # Run the ping command
-        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        
-        # Check the return code
-        if result.returncode == 0:
-            print(f"Ping to 8.8.{batt_number}.8 successful!")
-            print(result.stdout)
-        else:
-            print(f"Ping to 8.8.{batt_number}.8 failed!")
-            print(result.stderr)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+# Function to log errors to a file
+def log_error(error_message):
+    # Get the current time in a readable format
+    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+    # Specify the log file path (adjust the path as needed)
+    log_file = "networking_logs.log"
 
+    # Write the error message to the log file
+    with open(log_file, 'a') as log:
+        log.write(f"[{current_time}] {error_message}\n")
+
+def ping(batt_number, project, set, update_progressbar,pb):
+    if project == "All-In-One":
+        components_ip = {"MC2": f"194.0.8{batt_number}.111",
+                         "MICS": f"194.0.8{batt_number}.101",
+                         "MDB": f"194.0.8{batt_number}.121",
+                         "OC1": f"194.0.8{batt_number}.131"}
+    elif project == 'Spyder' and set == "Main":
+        components_ip = {"MC2": f"194.0.8{batt_number}.111",
+                         "MICS": f"194.0.8{batt_number}.101",
+                         "MDB": f"194.0.8{batt_number}.121",
+                         "OC1": f"194.0.8{batt_number}.131"}
+    elif project == 'Spyder' and set == "Backup":
+        components_ip = {"RC2": f"194.0.8{batt_number}.112",
+                         "RICS": f"194.0.8{batt_number}.102",
+                         "RDB": f"194.0.8{batt_number}.122",
+                         "OC2": f"194.0.8{batt_number}.132"}
+    elif project == 'Spyder' and set =='Main+Backup':
+        components_ip = {"MC2": f"194.0.8{batt_number}.111",
+                         "RC2": f"194.0.8{batt_number}.112",
+                         "MICS": f"194.0.8{batt_number}.101",
+                         "RICS": f"194.0.8{batt_number}.102",
+                         "MDB": f"194.0.8{batt_number}.121",
+                         "RDB": f"194.0.8{batt_number}.122",
+                         "OC1": f"194.0.8{batt_number}.131",
+                         "OC2": f"194.0.8{batt_number}.132"}   
+    else:
+        return False
+    
+    total_failure = 0
+    progressbar_value = 0
+    # Ping each component
+    for component, ip in components_ip.items():
+        command = ["ping", "-n", "2", ip]
+        print(f"Pinging {component} {ip}...")
+        try:
+            # Run the ping command
+            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            
+            # Check the return code and print result for each component
+            if result.returncode == 0:
+                print(f"Ping to {ip} successful for {component}!")
+                log_message = f"Ping succeeded for {component} ({ip}): {result.stderr}"
+                log_error(log_message)
+                progressbar_value += 25
+                update_progressbar(pb, progressbar_value)
+            else:
+                print(f"Ping to {ip} failed for {component}!")
+                error_message = f"Ping failed for {component} ({ip}): {result.stderr}"
+                log_error(error_message)                
+                print(result.stderr)
+                total_failure += 1
+                progressbar_value += 25
+                update_progressbar(pb, progressbar_value)
+        except Exception as e:
+            print(f"An error occurred while pinging {component}: {e}")
+            total_failure += 1
+    
+    return total_failure
